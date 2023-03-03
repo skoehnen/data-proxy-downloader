a53645f2744ea8a35c51cf8a1c51a83f398f96bc
Louisa Köhnen <s.koehnen@fz-juelich.de>
2023-03-03 23:04:42 +0100

    Add handling of args

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::env;
 mod data_proxy;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    dbg!(args);
     println!("Hello, world!");
     let stdin = io::stdin();
     let mut user_input = String::new();
