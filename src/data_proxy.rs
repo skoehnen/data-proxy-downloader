5e28e601a06b94c3694044b61baf2ebc6d7b6b9a
Louisa Köhnen <s.koehnen@fz-juelich.de>
2023-03-09 14:37:18 +0100

    Read status of bucket successfully

--- src/data_proxy.rs
+++ src/data_proxy.rs
@@ -2,12 +2,25 @@ extern crate fstrings;
 use fstrings::format_args_f;
 
 use reqwest::blocking::Client;
-use reqwest::Error;
-use reqwest::header::HeaderValue;
+//use reqwest::Error;
+//use reqwest::header::HeaderValue;
+
+use serde::{Deserialize, Serialize};
 
 pub struct Bucket {
     pub token: String,
     pub id: String
+}
+
+#[derive(Debug, Deserialize)]
+struct Status {
+    name: String,
+    objects_count: u64,
+    bytes: u64,
+    last_modified: String,
+    is_public: bool,
+    role: String,
+    is_initialized: bool,
 }
 
 pub fn test() {
@@ -25,18 +38,18 @@ pub fn get_stat(bucket: Bucket) {
 
     //let val = HeaderValue::from_str("Hallo Tim");
 
-    let val = HeaderValue::from_str(&format!("Bearer {}", bucket.token.clone()));
+    //let val = HeaderValue::from_str(&format!("Bearer {}", bucket.token.clone()));
 
-    dbg!(val);
+    //dbg!(val);
 
-    //let request = client
-    //    .get(stats_url)
-    //    .header(reqwest::header::AUTHORIZATION, val);
+    let response = client
+        .get(stats_url)
+        .bearer_auth(bucket.token)
+        .send()
+        .unwrap();
 
-    //println!("{:?}", request);
-    //dbg!(request);
+    //dbg!(response.text());
+    let status: Status = response.json::<Status>().unwrap();
 
-    //let response = request.send();
-
-    //println!("{:?}", response);
+    dbg!(status);
 }
